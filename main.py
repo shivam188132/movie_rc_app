1a2ea50bb07c1b7c5c4ead1ab38ddad83193487e
shivam kumar <129589858+shivam188132@users.noreply.github.com>
2024-06-08 01:49:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ if st.button('Recommended'):
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(names[0])
-        st.image("https://static.streamlit.io/examples/cat.jpg")
+        st.image(posters[0])
     with col2:
         st.text(names[1])
         st.image(posters[1])
